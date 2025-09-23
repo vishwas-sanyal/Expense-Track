989b60327b93bf8512052e704ec7379b56722ef1
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-09-23 17:54:52 +0530

    Initial commit

--- Expense-Track/vite.config.js
+++ Expense-Track/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "Expense-Track",
+        description: "A simple and fast Expense Tracker app",
+        theme_color: "#000",
+        background_color: "#252525",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
